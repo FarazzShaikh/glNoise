09e7fe340efc011d60d02a3c2d4ba9692112be99
Faraz Shaikh <farazzshaikh@gmail.com>
2021-07-15 11:38:18 +0530

    Patched bugs

--- scripts/patchModule.sh
+++ scripts/patchModule.sh
@@ -2,9 +2,9 @@
 
 cat build/glNoise.m.node.js > temp
 
-echo "\n" > build/glNoise.m.js
+echo "" > build/glNoise.m.js
 sed -n '1,/\/\/~START~/p;/\/\/~END~/,$p' temp >> build/glNoise.m.js
 cat build/glNoise.m.js > temp
-echo "\n" > build/glNoise.m.js
+echo "" > build/glNoise.m.js
 sed 's/nodeFetch/false/g' temp >> build/glNoise.m.js
 rm -rf temp
